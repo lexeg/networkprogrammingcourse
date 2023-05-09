b893a7312343bd4cabd887a9603883fcc14f59a4
apetrenko <apetrenko@gseis.ru>
2023-05-09 21:15:08 +0500

    Добавил примеры для урока 1.
    Добавил серверную реализацию
    Добавил клиентскую реализацию

---
+++ Lesson1/ClientWpfApp/ClientWpfApp/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows;
+
+namespace ClientWpfApp;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void LoadColumbiaEduPageClick(object sender, RoutedEventArgs e)
+    {
+        var url = new Uri("http://www.columbia.edu/~fdc/sample.html");
+        var query = $@"GET {url.AbsoluteUri} HTTP/1.0
+Host: {url.Host}
+Connection: Close
+
+";
+        var socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+        try
+        {
+            socket.Connect(url.Host, url.Port);
+            socket.Send(Encoding.ASCII.GetBytes(query));
+            var buffer = new byte[1024];
+            int length;
+            do
+            {
+                length = socket.Receive(buffer);
+                TextBox1.Text += Encoding.ASCII.GetString(buffer, 0, length);
+            } while (length > 0);
+        }
+        finally
+        {
+            socket.Shutdown(SocketShutdown.Both);
+            socket.Close();
+        }
+    }
+
+    private void ConnectToServerClick(object sender, RoutedEventArgs e)
+    {
+        var ip = IPAddress.Parse("127.0.0.1");
+        var socket = new Socket(SocketType.Stream, ProtocolType.IP);
+        try
+        {
+            socket.Connect(ip, 1_024);
+            socket.Send(Encoding.ASCII.GetBytes($"Hello server. {DateTime.Now}"));
+            socket.Shutdown(SocketShutdown.Send);
+            var buffer = new byte[1_024];
+            int length;
+            do
+            {
+                length = socket.Receive(buffer);
+                TextBox1.Text += Encoding.ASCII.GetString(buffer, 0, length);
+            } while (length > 0);
+
+            TextBox1.Text += Environment.NewLine;
+        }
+        finally
+        {
+            socket.Shutdown(SocketShutdown.Both);
+            socket.Close();
+        }
+    }
+}
---
+++ Lesson1/ServerConsoleApp/ServerConsoleApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+var serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+
+var address = IPAddress.Parse("127.0.0.1");
+var endpoint = new IPEndPoint(address, 1024);
+
+serverSocket.Bind(endpoint);
+serverSocket.Listen(10);
+try
+{
+    while (true)
+    {
+        var clientSocket = serverSocket.Accept();
+        Console.WriteLine(clientSocket.RemoteEndPoint);
+        var buffer = new byte[1_024];
+        int length;
+        do
+        {
+            length = clientSocket.Receive(buffer);
+            Console.Write(Encoding.ASCII.GetString(buffer, 0, length));
+        } while (length > 0);
+
+        Console.WriteLine();
+        var message = $"Hello client. Time {DateTime.Now}";
+        clientSocket.Send(Encoding.ASCII.GetBytes(message));
+        clientSocket.Shutdown(SocketShutdown.Both);
+        clientSocket.Close();
+    }
+}
+catch (SocketException ex)
+{
+    Console.WriteLine(ex.Message);
+}
